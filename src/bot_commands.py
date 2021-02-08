d03997a1cd5d9690386b201c9879c07db7360e97
Henry Valbuena <dev.henryvm@gmail.com>
2021-02-08 12:00:02 -0500

    removed unused commands

--- src/bot_commands.py
+++ src/bot_commands.py
@@ -31,31 +31,9 @@ class Greetings(commands.Cog):
         self.bot = bot
         logger.debug(f"{self.__class__.__name__} initialized")
 
-    @commands.command()
+    @commands.command(aliases=["hi", "hey"])
     async def salut(self, ctx):
         await ctx.send(f"Hello {ctx.author}")
-
-
-class Math(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-        logger.debug(f"{self.__class__.__name__} initialized")
-
-    @commands.command()
-    async def sum(self, ctx, a: int, b: int):
-        await ctx.send(f"Sum {a} + {b} = {a + b}")
-
-    @commands.command()
-    async def sub(self, ctx, a: int, b: int):
-        await ctx.send(f"Sub {a} - {b} = {a - b}")
-
-    @commands.command()
-    async def div(self, ctx, a: int, b: int):
-        await ctx.send(f"Div {a} / {b} = {a / b}")
-
-    @commands.command()
-    async def mul(self, ctx, a: int, b: int):
-        await ctx.send(f"Mul {a} * {b} = {a * b}")
 
 
 class Tests(commands.Cog):
@@ -70,7 +48,7 @@ class Tests(commands.Cog):
                 {
                     "title": "Server Response",
                     "body": "Press !st\nThen after that, you should be able to\ndepear lorem ipsum",
-                    "link": "https://www.google.com"
+                    "link": "https://www.google.com",
                 }
             )
         )
@@ -78,13 +56,7 @@ class Tests(commands.Cog):
     @commands.command()
     async def echo(self, ctx, title, body, link=None):
         await ctx.send(
-            MD.card(
-                {
-                    "title": title,
-                    "body": body,
-                    "link": "" if link is None else link
-                }
-            )
+            MD.card({"title": title, "body": body, "link": "" if link is None else link})
         )
 
     @commands.command(aliases=["em", "emb"])
@@ -93,10 +65,10 @@ class Tests(commands.Cog):
             "title": "Terminator",
             "description": "A human soldier is sent from 2029 to 1984 to stop an almost indestructible cyborg killing machine, sent from the same year, which has been programmed to execute a young woman whose unborn son is the key to humanity's future salvation.",
             "url": "https://www.imdb.com/title/tt0088247/",
-            "thumbnail": ""
+            "thumbnail": "",
         }
 
         await ctx.send(embed=MD.embed(**struct))
 
 
-COGS = [Math, Greetings, CommandErrorHandler, Status, Tests]
+COGS = [Greetings, CommandErrorHandler, Status, Tests]
